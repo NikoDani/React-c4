d417b0c57bbaf9f074b02ba450ec6ba0b79a0509
Datodia <dato.diasamidze.02@gmail.com>
2026-03-15 16:19:47 +0400

    add homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Homepage from './components/homepage'
 
 function App() {
 
   return (
     <>
       <div>
-        <h1>Homepage</h1>
+        <Homepage />
         <h2>quiz page</h2>
       </div>
     </>
